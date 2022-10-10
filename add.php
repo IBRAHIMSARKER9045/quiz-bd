1b05b33f960d7d358372e0e8c6bf789e9255ab2e
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-10 23:12:11 +0600

    quiz add

---
+++ add.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require __DIR__ . '/vendor/autoload.php';
+require "database.php";
+?>
+<?php require "configuration.php"; ?>
+<?php require "inc/header.php"; ?>
+<div class="container-fluid  mt-5">
