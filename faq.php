e9bb157e419e62920443fc6fdb9f15ccdec9b35a
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-27 21:29:48 +0600

    faq some change

--- faq.php
+++ faq.php
@@ -15,3 +15,16 @@ require "database.php";
    <div class="row pt-5 pb-5">
       <div class="pt-5 pb-5">
          <h1 class="text-info">FAQ</h1>
+         </div>
+      <div class="accordion pb-5 " id="accordionExample">
+         <div class="accordion-item pb-5 text-white bg-info">
+            <?php
+            $query = "SELECT message,reply FROM `contact` limit 3";
+            $queryResult = $conn->query($query);
+            $q = $queryResult->num_rows;
+
+            while ($row = $queryResult->fetch_assoc()) {
+            ?>
+               <?php echo '<div class="accordion-header" id="faq">
+          <button class="accordion-button text-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <h6>' . $row['message'] . '</h6>
