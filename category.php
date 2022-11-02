820785d4bdfedc14f23079c30ea3b4dd4f725962
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-11-02 22:14:22 +0600

    category

---
+++ category.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require __DIR__ . '/vendor/autoload.php';
+require "database.php";
+?>
+<?php require "configuration.php"; ?>
+<?php require "inc/header.php"; ?>
+<div class="container-fluid text-center mt-5">
+  <div class="row bg-light">
+    <div class="pt-5">
+      <h1 class="text-info">ALL CATEGORIES</h1>
+    </div>
+    <div class="col-md-12 mt-5">
+      <div class="card pt-5 pb-5 bg-light">
